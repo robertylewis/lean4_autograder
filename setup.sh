6c5ab3a1b996f8e096c069245975d3cba4179ada
Rob Lewis <rob.y.lewis@gmail.com>
2023-01-18 01:24:14 -0500

    trim down main autograder

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+AUTOGRADER_REPO="https://github.com/robertylewis/hw1_autograder"
+
 curl https://raw.githubusercontent.com/leanprover/elan/master/elan-init.sh -sSf | sh -s -- -y --default-toolchain leanprover/lean4:nightly-2023-01-16
 
 # ~/.elan/bin/elan default leanprover/lean4:nightly
@@ -10,10 +12,15 @@ apt-get install -y python3 python3-pip python3-dev
 
 cd /autograder/source
 
-~/.elan/bin/lake update 
+git init 
+git remote add origin $AUTOGRADER_REPO
+git fetch origin 
+git reset --hard origin/master
+
+# ~/.elan/bin/lake update 
 
 ~/.elan/bin/lake exe cache get 
 
-~/.elan/bin/lake clean
+# ~/.elan/bin/lake clean
 
-~/.elan/bin/lake build autograder AutograderTests 
+# ~/.elan/bin/lake build autograder AutograderTests 
